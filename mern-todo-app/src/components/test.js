cc503fdc074fb5f07c45d60c2bf57b5f927c184e
xiandengjin <lionking921117@gmail.com>
2020-04-26 14:38:44 +0800

    initial commit

---
+++ mern-todo-app/src/components/test.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class TodosList extends Component {
+  render() {
+    <div>
+      {" "}
+      <p> welcome to edit to component </p>{" "}
+    </div>;
+  }
+}
